b427738642a40ff18cf37cef28930a0e4f5ec839
hyeonjin0209 <jonmoshyeonmin@gmail.com>
2025-07-09 17:43:25 +0900

    feat : Stage Select UI
    ddddd

--- Assets/Team Tomodachi/HJ/Scripts/System/StageManager.cs
+++ Assets/Team Tomodachi/HJ/Scripts/System/StageManager.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class StageManager : MonoBehaviour
 {
     private bool _isNearPlayer = false;
     [SerializeField] private GameObject _stageSelectUI;
+    private Outline _outLine;
 
+    private void Awake()
+    {
+        _outLine = GetComponent<Outline>();
+    }
+    private void Start()
+    {
+        _outLine.enabled = true;
+    }
     private void Update()
     {
         if (_isNearPlayer == true && Keyboard.current.fKey.wasPressedThisFrame)
